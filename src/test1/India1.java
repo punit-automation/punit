b270c4d470ed23615cc04aaea0af4d69ed85d15f
Punit Shukla <punit.shukla1@gmail.com>
2021-04-18 17:48:48 +0530

    Add all Testing GIT

--- src/test1/India1.java
+++ src/test1/India1.java
@@ -5,7 +5,9 @@ public class India1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Word");
-	}
+				System.out.println("Hello Word");
+		}
 
+	
+	
 }
